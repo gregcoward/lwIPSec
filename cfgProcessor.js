be9c4473951f9c99cf8ce35fe03d445fc55ce1e7
gregcoward <g.coward@f5.com>
2019-12-02 20:59:51 -0800

    Update cfgProcessor.js

--- cfgProcessor.js
+++ cfgProcessor.js
@@ -1,6 +1,7 @@
 const http = require('http');
-const hostname = '206.124.129.60';
-const port = 5000;
+var hostname = 'localhost';
+const port = 8443;
+var myArgs = process.argv.slice(2);
 var fs = require("fs");
 var exec = require('child_process').exec;
 var Validator = require('jsonschema').Validator;
@@ -25,32 +26,44 @@ var schema = {
     ]
 };
 
+//Expect a single parameter (hostname) - defaults to 127.0.0.1 
+if (myArgs.length == 0) {
+    hostname = '127.0.0.1';
+} else {
+    hostname = myArgs[0];
+};
+
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
 const server = http.createServer((req, res) => {
+    
+    // Validate required http method (POST) was used
     if (req.method == 'POST') {
-        
         let body = '';
+        // Capture post body
         req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
+            body += chunk.toString(); // Convert Buffer to string
         });
         req.on('end', () => {
-            try {
- 
+            // Validate that input data is valid JSON including required key/value pairs
+            try { 
                 reqObj = JSON.parse(body);
                 v.addSchema(schema, '/tunnelCfg');
                 var results = v.validate(reqObj, schema);
+                
+                // If JSON is valid begin file conf file creations and restart IPSec daemon
                 if (results.errors.length == 0) {
                     
-                    //Move to separate function - secFileCreate
+                    // Create ipsec.secrets file 
                     var createStream = fs.createWriteStream("/etc/ipsec.secrets");
-                    console.log(reqObj.preSharedKey);
                     createStream.write(reqObj.sourceAddress + ' ' + reqObj.destinationAddress1 + ' : PSK "' + reqObj.preSharedKey + '"\r');
                     if (reqObj.destinationAddress2 != null) { 
                         createStream.write(reqObj.sourceAddress + ' ' + reqObj.destinationAddress2 + ' : PSK "' + reqObj.preSharedKey + '"\r');
                     };
                     createStream.end();
-                    // End of separate function
+                    // End of file creation
 
-                    //Move to separate function - tunnelFileCreate
+                    // Create cons.conf file  - is referenced by /etc/ipsec.conf
                     var createStream2 = fs.createWriteStream("/etc/ipsec.d/cons.conf");
                     createStream2.write('\n\nconn premtoazure');
                     createStream2.write('\n left=' + reqObj.sourceAddress);      
@@ -67,17 +80,33 @@ const server = http.createServer((req, res) => {
                         createStream2.write('\n rightsubnet=' + reqObj.destinationSubnet);
                     };
                     createStream2.end();
-                    // End of separate function 
+                    // End of file creation
                     
+                    // Restart IPSec daemon
                     function puts(error, stdout, stderr) { sys.puts(stdout) }
-                    exec("sudo ipsec restart", function(err, stdout, stderr) {
-                       console.log(stdout);
+                    exec("ipsec restart", function(err, stdout, stderr) {});
+                    
+                    // Output ipsec status to console
+                    sleep(5000).then(() => {
+                        // This will execute 10 seconds from now
+                        exec("ipsec status", function(err, stdout, stderr) {
+                            let ts = Date.now();
+                            let date_ob = new Date(ts);
+                            let date = date_ob.getDate();
+                            let month = date_ob.getMonth() + 1;
+                            let year = date_ob.getFullYear();
+                            console.log('Entry Date - Timestamp: ' + month + "/" + date + "/" + year + ' - ' + ts);
+                            console.log(stdout);
+                        });
                     });
 
+                    // Reply to caller with success
                     res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write('Success: Configuration Updated');
+                    res.write('The IPSec deployment Updated');
                     res.end();
                 } else {
+
+                    // The JSON package was malformed - notify caller
                     res.writeHead(400, {'Content-Type': 'text/html'});
                     res.write('Malformed JSON');
                     console.log(results.errors);
@@ -94,9 +123,9 @@ const server = http.createServer((req, res) => {
         });
     }
     else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        // POST method not used - Return 405 to caller
+        res.writeHead(405, {'Content-Type': 'text/html'});
         res.write('Invalid HTTP Method');
-        console.log(req.method);
         res.end();
 };
 });
